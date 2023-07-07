f5e8f46c661d041447c55b572d3506be5482126b
Arthur Dos Santos Almeida <arthur.almeida@g.globo>
2023-07-07 15:39:35 -0300

    excluindo arquivo

--- aprendizado-nao-supervisionado/recomendacao/filtragem-baseada-em-conteudo/recomendacao.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LinearRegression
-
-filmes = pd.read_csv("../movies.csv")
-notas = pd.read_csv("../ratings.csv")
-
-vectorizer = TfidfVectorizer()
-#X = vectorizer.fit_transform(filmes[""])
